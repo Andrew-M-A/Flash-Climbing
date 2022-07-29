36722fbf1b29545943afaa7eb877dc0fdd37b2a0
Andrew Almeida <am.almeida000@gmail.com>
2022-07-29 01:00:34 +0000

    in progress

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 export default function HomeMap() {
@@ -8,15 +8,40 @@ export default function HomeMap() {
   if (!isLoaded) {
     return <div>Loading...</div>;
   }
-  return <Map/>;
+  return (
+  <>
+  <Map/>
+  <Locate/>
+  </>
+  );
 }
 
+// golden palace: lat: 34.04055, lng: -118.46473
+
 function Map() {
-  const center = useMemo(() => ({ lat: 34.04055, lng: -118.46473 }), []);
+  const center = useMemo(() => ({ lat: 33.63487, lng: -117.74045 }), []);
   return <GoogleMap
-  zoom={10}
-  center={{ lat: 34.04055, lng: -118.46473 }}
+  onDragEnd={HandleDrag}
+  zoom={11}
+  center={{ lat: 33.63487, lng: -117.74045 }}
   mapContainerClassName = 'map-container'>
     <Marker position={center} />
   </GoogleMap>;
+}
+
+function Locate() {
+  const [lat, setLat] = useState([]);
+  const [lng, setLng] = useState([]);
+  React.useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      setLat(position.coords.latitude);
+      setLng(position.coords.longitude);
+    });
+    // console.log('Latitude is: ', lat);
+    // console.log('Longitude is: ', lng);
+  }, [lat, lng]);
+}
+
+function HandleDrag() {
+  // console.log(navigator);
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import HelloWorld from '../components/hello-world';
+// import HelloWorld from '../components/hello-world';
+import HomeMap from '../components/map';
 
 export default function Home(props) {
   return (
     <div>
-      <HelloWorld />
+      <HomeMap />
     </div>
   );
 }
