176f0da76877e11c12809da74ff3bc70f4bcd342
Andrew Almeida <am.almeida000@gmail.com>
2022-07-30 19:18:36 +0000

    getting business info from yelp...

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 
+const yelp = require('yelp-fusion');
+const client = yelp.client(process.env.YELP_API_KEY);
+
 const app = express();
 const publicPath = path.join(__dirname, 'public');
 
@@ -17,6 +20,21 @@ app.get('/api/hello', (req, res) => {
 });
 
 app.use(errorMiddleware);
+
+app.get('/api/nearby', (req, res, next) => {
+  client.search({
+    term: 'climbing',
+    latitude: 33.6349171,
+    longitude: -117.7405465,
+    radius: 1000
+  })
+    .then(response => {
+      res.json(response);
+    }).then(data => res.json(data))
+    .catch(e => {
+      res.status(400);
+    });
+});
 
 app.listen(process.env.PORT, () => {
   process.stdout.write(`\n\napp listening on port ${process.env.PORT}\n\n`);
